e124c12a37320b1a487840a8088372e9eb4f7a41
SemihKC94 <semihkcetin@mail.com.tr>
2021-07-13 20:29:46 +0300

    Basic Car System with ESP Configuration

---
+++ Assets/SKC Starter/Scripts/CarSystem/SKC_CarCam.cs
@@ -0,0 +1,38 @@
+﻿/*//////////////////////////////////////////////////////////////////////////////////////////
+//      █─▄▄▄▄█▄─█─▄█─▄▄▄─█                                                               //
+//      █▄▄▄▄─██─▄▀██─███▀█             Scripts created by Semih Kubilay Çetin            //
+//      ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀                                                               //
+//////////////////////////////////////////////////////////////////////////////////////////*/
+using UnityEngine;
+
+public class SKC_CarCam : MonoBehaviour
+{
+    [Header("Cam Configuration")]
+    public Vector3 offSet;
+    public Transform target;
+    public float translateSpeed;
+    public float rotationSpeed;
+
+    private void FixedUpdate()
+    {
+        HandleTranslation();
+        HandleRotation();
+    }
+
+    private void HandleTranslation()
+    {
+        var targetPosition = target.TransformPoint(offSet);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.fixedDeltaTime);
+    }
+
+    private void HandleRotation()
+    {
+        var direction = target.position - transform.position;
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.fixedDeltaTime);
+    }
+}
+
+/* Tip    #if UNITY_EDITOR
+          Debug.Log("Unity Editor");
+          #endif                          Tip End */
